e2f11928b92f01278190e7250f83b96289dc3577
Neil Murphy <wnmurphy@gmail.com>
2016-04-03 10:22:28 -0700

    Code cleanup and add readme.

---
+++ README.md
@@ -0,0 +1,6 @@
+Twittler
+=====================
+
+This repo contains a Twitter clone I built.
+
+data_generator.js creates random madlib-style tweets, and the script in index.html continually checks for new tweets and renders them to the page using jQuery.
--- data_generator.js
+++ data_generator.js
@@ -1,8 +1,3 @@
-/*
- * NOTE: This file generates fake tweet data, and is not intended to be part of your implementation.
- * You can safely leave this file untouched, and confine your changes to index.html.
- */
-
 // set up data structures
 window.streams = {};
 streams.home = [];
@@ -14,14 +9,14 @@ streams.users.douglascalhoun = [];
 window.users = Object.keys(streams.users);
 
 // utility function for adding tweets to our data structures
-var addTweet = function(newTweet){
+var addTweet = function (newTweet) {
   var username = newTweet.user;
   streams.users[username].push(newTweet);
   streams.home.push(newTweet);
 };
 
 // utility function
-var randomElement = function(array){
+var randomElement = function (array) {
   var randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 };
@@ -33,12 +28,12 @@ var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the
 var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
 var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
 
-var randomMessage = function(){
+var randomMessage = function () {
   return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
 };
 
 // generate random tweets on a random schedule
-var generateRandomTweet = function(){
+var generateRandomTweet = function () {
   var tweet = {};
   tweet.user = randomElement(users);
   tweet.message = randomMessage();
@@ -46,11 +41,11 @@ var generateRandomTweet = function(){
   addTweet(tweet);
 };
 
-for(var i = 0; i < 10; i++){
+for (var i = 0; i < 10; i++) {
   generateRandomTweet();
 }
 
-var scheduleNextTweet = function(){
+var scheduleNextTweet = function () {
   generateRandomTweet();
   setTimeout(scheduleNextTweet, Math.random() * 15000);
 };
@@ -58,8 +53,8 @@ scheduleNextTweet();
 
 // utility function for letting students add "write a tweet" functionality
 // (note: not used by the rest of this file.)
-var writeTweet = function(message){
-  if(!visitor){
+var writeTweet = function (message) {
+  if (!visitor) {
     throw new Error('set the global visitor property!');
   }
   var tweet = {};
